fe43c74886a9cf33036e959eb72d27309d8f8b9b
joseph preston <98495342+emptyspacer@users.noreply.github.com>
2022-04-20 10:38:14 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	useful ""
+)
+
+func main() {
+	useful.permutations()
+}
---
+++ permutations.go
@@ -0,0 +1,9 @@
+package useful
+
+import (
+  "fmt"
+)
+
+func permutations(
+  fmt.Println("hello world")
+)
